400a67942007b2960a025eb12afc5cd157ea586e
Jan Nowakowski <janeknowakowski9@gmail.com>
2026-09-23 10:50:06 +0200

    refactor(android): remove unnecessary commit

--- packages/enriched-markdown-android/compose/src/main/java/com/swmansion/enriched/markdown/compose/EnrichedMarkdownText.kt
+++ packages/enriched-markdown-android/compose/src/main/java/com/swmansion/enriched/markdown/compose/EnrichedMarkdownText.kt
@@ -21,7 +21,6 @@ import com.swmansion.enriched.markdown.parser.Md4cFlags as Md4cFlagsInternal
 
 typealias Md4cFlags = Md4cFlagsInternal
 
-/** The item a task-list checkbox toggle happened on, and its checked state after the toggle. */
 typealias TaskListItemToggle = TaskListItemToggleInternal
 
 /**
